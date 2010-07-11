159ef6cc8a031a8f0d70029601bf7f3e374c4d1f
Joe Kulig <kulandthegang@gmail.com>
2010-07-10 21:13:23 -0400

    WIP

--- src/ZipFile.cpp
+++ src/ZipFile.cpp
@@ -8,6 +8,27 @@
 #include <iostream>
 #include <fstream>
 
-ZipFile::ZipFile(ifstream& stream, ZipLocalFileHeader *fileHeader) {
+/**
+ * @param stream reference to input file stream. Should be pointing to the file name field
+ * @param fileHeader current zip file being processed
+ *
+ */
+ZipFile::ZipFile(ifstream& stream, ZipLocalFileHeader& fileHeader) {
+	char fileName[255];
 
+	// read in file name
+	stream.read(fileName, fileHeader.fileNameLen);
+	// initialize name
+	name = string(fileName);
+	size = uncompressesdSize;
+	// read in the compressed data
+	if (compressedSize > 0) {
+		data = malloc(compressedSize);
+		if (data == NULL) {
+			cerr << "ZipFile: failed to malloc: " << compressedSize;
+			exit(-1);
+		}
+		stream.read(data, compressedSize);
+	}
+	cout << name << ' ' << size << ' ' << compressedSize << endl;
 }
