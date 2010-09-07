74e5b23c4557ff70392486093bc3a6476f2b9535
Joe Kulig <kulandthegang@gmail.com>
2010-09-06 21:39:35 -0400

    Inflation works

--- src/ZipFile.cpp
+++ src/ZipFile.cpp
@@ -9,14 +9,14 @@
 #include <fstream>
 
 #include <stdlib.h>
-//extern "C" {
 #include <zlib.h>
-// }
+#include <string.h>
 
 #include "ZipFile.h"
 
 using namespace std;
 
+#define WBITS	15		// window bits
 /**
  * @param stream reference to input file stream. Should be pointing to the file name field
  * @param fileHeader current zip file being processed
@@ -31,6 +31,7 @@ ZipFile::ZipFile(ifstream& stream, ZipLocalFileHeader& fileHeader) {
 	// initialize name
 	name = string(fileName);
 	size = fileHeader.uncompressedSize;
+	compressedSize = fileHeader.compressedSize;
 	compression = fileHeader.compression;
 	// read in the compressed data
 	if (fileHeader.compressedSize > 0) {
@@ -44,23 +45,65 @@ ZipFile::ZipFile(ifstream& stream, ZipLocalFileHeader& fileHeader) {
 	cout << name << ' ' << fileHeader.compression << ' ' << size << ' ' << fileHeader.compressedSize << endl;
 }
 
+#define CHUNK	(256*1024)
+
 uint8_t *ZipFile::inflate() {
-	uint8_t *infData;
+	uint8_t *infData, *outBuffer;
 	z_stream strm;
-	uLongf destBufSize;
+	int bytesLeft;
 
 	if(size == 0)
 		return NULL;
 
 	infData = (uint8_t *)malloc(size);
-	destBufSize = size;
-	int ret = uncompress((Bytef *)infData, (uLongf *)&destBufSize, (const Bytef *)data, compressedSize);
+	outBuffer = (uint8_t *)malloc(CHUNK);
+
+	// data no deflated, so just copy over
+	if(compression == CM_STORED) {
+		memcpy(infData, data, size);
+		return infData;
+	}
+
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.avail_in = compressedSize;
+	strm.next_in = (Bytef *)data;
+//	printf("data: %02x %02x %02x\n", *data, *(data+1), *(data+2));
+	int ret = inflateInit2(&strm, -WBITS);
 
 	if(ret != Z_OK) {
 		cout << "inflate: " << zError(ret) << endl;
 		free(infData);
 		return (uint8_t *)-1;
 	}
+
+	strm.avail_in = compressedSize;
+	strm.next_in = (Bytef *)data;
+	int processed_out = 0;
+	while(ret != Z_STREAM_END) {
+		strm.next_out = (Bytef *)outBuffer;
+		strm.avail_out = CHUNK;
+		cout << "out: " << strm.avail_out << " " << hex << (int)strm.next_out << " processed out: " << dec << processed_out
+				<< " in: " << hex << (int)strm.next_in << " " << dec << strm.avail_in << endl;
+		ret = ::inflate(&strm, Z_SYNC_FLUSH);
+		switch(ret) {
+		case Z_NEED_DICT:
+		case Z_MEM_ERROR:
+		case Z_DATA_ERROR:
+			cout << "inflate loop: " << zError(ret) << endl;
+			::inflateEnd(&strm);
+			return (uint8_t *)-1;
+		case Z_STREAM_END:
+			break;
+		default:
+			cout << "return: " << zError(ret) << endl;
+		}
+		int processed_in = compressedSize - strm.avail_in;
+		processed_out = CHUNK - strm.avail_out;
+		strm.next_in += processed_in;
+	}
+	::inflateEnd(&strm);
+	memcpy(infData, outBuffer, size);
 	cout << "Inflated --> " << name << endl;
 	return infData;
 }
