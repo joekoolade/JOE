8f407d414dc8e22eced7f903058b462b84b7fd56
Joe Kulig <kulandthegang@gmail.com>
2013-09-06 11:28:34 -0400

    Remove target/ and ignore
    Signed-off-by: Joe Kulig <kulandthegang@gmail.com>

--- jikesrvm-hg/target/development_x86_64-linux/c/RunBootImage.h
+++
@@ -1,8 +0,0 @@
-
-      const unsigned heap_default_initial_size = 50*1024*1024;
-      const unsigned heap_default_maximum_size = 100*1024*1024;
-      const char *rvm_version = "Jikes RVM 3.1.3+hg (revision 50c500c6d5b8ee2c30fc985d4e93d16ed14fb6f1)";
-      const char *rvm_configuration = "development";
-      const char *rvm_host_configuration = "/home/jkulig/git/jei/jikesrvm-hg/build/configs/development.properties";
-      const char *rvm_target_configuration = "/home/jkulig/git/jei/jikesrvm-hg/build/targets/x86_64-linux.properties";
-    
