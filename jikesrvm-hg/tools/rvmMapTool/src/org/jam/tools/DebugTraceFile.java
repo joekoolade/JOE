09abaf8bb5cf1b67d74d7598b60f137f558acb79
Joe Kulig <kulandthegang@gmail.com>
2014-11-18 17:53:39 -0500

    implementation of the process dump file
    Signed-off-by: Joe Kulig <kulandthegang@gmail.com>

--- jikesrvm-hg/tools/rvmMapTool/src/org/jam/tools/DebugTraceFile.java
+++ jikesrvm-hg/tools/rvmMapTool/src/org/jam/tools/DebugTraceFile.java
@@ -1,7 +1,12 @@
 package org.jam.tools;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class DebugTraceFile {
@@ -14,32 +19,39 @@ public class DebugTraceFile {
 	        
 	        traceFile.processArguments(args);
 	        traceFile.processFile();
-	        traceFile.openTraceFile();
-        } catch (FileNotFoundException e) {
+	    } catch (FileNotFoundException e) {
 	        System.out.print(e.getMessage());
 	        e.printStackTrace();
         }
 	}
 
-	private File traceFile;
-	private String traceFileName;
+	private String dumpFileName;
+	private String debuggedFileName="trace.log";
 	
-	void openTraceFile() {
-		traceFile = new File(traceFileName);
-	}
-
-	void processTraceFile() throws FileNotFoundException {
-		Scanner scanFile = new Scanner(traceFile);
+	void processDumpFile() throws IOException {
+		BufferedReader dumpFile = new BufferedReader(new FileReader(dumpFileName));
+		BufferedWriter tracedFile = new BufferedWriter(new FileWriter(debuggedFileName));
+		String line;
 		
+		while(dumpFile.ready()) {
+			line = dumpFile.readLine();
+			if(line.equals("----------------")) {
+				String inToken = dumpFile.readLine();
+				if(inToken.equals("IN:")) {
+					String asmLine = dumpFile.readLine();
+					
+				}
+			}
+		}
 	}
 	private void processFile() {
     }
 
 	void processArguments(String[] args) {
 		if(args.length == 0) {
-			traceFileName = "stderr.txt";
+			dumpFileName = "stderr.txt";
 		} else {
-			traceFileName = args[0];
+			dumpFileName = args[0];
 		}
     }
 }
