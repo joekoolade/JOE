49116a1303e35c29ef91501a8a0eb65294c8739f
Joseph Kulig <joseph.kulig@gmail.com>
2016-11-02 14:16:02 -0400

    Set stack to 4096
    Signed-off-by: Joseph Kulig <joseph.kulig@gmail.com>

--- jikesrvm-hg/rvm/src/org/jikesrvm/scheduler/IdleThread.java
+++ jikesrvm-hg/rvm/src/org/jikesrvm/scheduler/IdleThread.java
@@ -6,7 +6,10 @@
  */
 package org.jikesrvm.scheduler;
 
+import static org.jikesrvm.ia32.StackframeLayoutConstants.STACK_SIZE_NORMAL;
+
 import org.jikesrvm.VM;
+import org.jikesrvm.mm.mminterface.MemoryManager;
 import org.jikesrvm.runtime.Magic;
 import org.vmmagic.pragma.NonMoving;
 import org.vmmagic.unboxed.ObjectReference;
@@ -18,33 +21,34 @@ import org.vmmagic.unboxed.ObjectReference;
 @NonMoving
 public class IdleThread extends SystemThread {
 
-    /**
+  /**
      * 
      */
-    public IdleThread()
-    {
-        super("IdleThread");
-        RVMThread.idleThread = this.rvmThread;
-        VM.sysWriteln("idle thread: ", ObjectReference.fromObject(RVMThread.idleThread));
-    }
-    
-    /* 
-     * This is the idling loop when there is nothing else
-     * to be done
-     */
-    @Override
-    public void run()
-    {
-        VM.sysWriteln("Starting the Idle Thread");
-        /*
-         * For now just pause because we will be doing is spinning until a process becomes available.
-         * Later on may want to do some power saving stuff.
-         */
-        while(true)
-        {
-            Magic.pause();
-        }
+  public IdleThread()
+  {
+    super(MemoryManager.newStack(4096), "IdleThread");
+    RVMThread.idleThread = this.rvmThread;
+    VM.sysWriteln("idle thread: ", ObjectReference.fromObject(RVMThread.idleThread));
+  }
 
+  /*
+   * This is the idling loop when there is nothing else
+   * to be done
+   */
+  @Override
+  public void run()
+  {
+    VM.sysWriteln("Starting the Idle Thread");
+    /*
+     * For now just pause because we will be doing is spinning until a process becomes available.
+     * Later on may want to do some power saving stuff.
+     */
+    while (true)
+    {
+      // rvmThread.checkBlock();
+      Magic.pause();
     }
+
+  }
 
 }
