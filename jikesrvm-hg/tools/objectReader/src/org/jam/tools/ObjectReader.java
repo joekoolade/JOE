aeb5dcc23d95dfb3e7520ba7050ebe5d2127ed28
Joe Kulig <kulandthegang@gmail.com>
2018-07-30 06:26:50 -0400

    WIP

--- jikesrvm-hg/tools/objectReader/src/org/jam/tools/ObjectReader.java
+++ jikesrvm-hg/tools/objectReader/src/org/jam/tools/ObjectReader.java
@@ -15,6 +15,7 @@ public class ObjectReader
     private int jtoc;
     private int stack;
     private int bootThread;
+    private int tibType;
     
     static final private int BOOT_PARAMETERS_ADDRESS = 0x001000E0;
     
@@ -37,12 +38,27 @@ public class ObjectReader
         jtoc = memory.read(BOOT_PARAMETERS_ADDRESS);
         stack = memory.read(BOOT_PARAMETERS_ADDRESS+4);
         bootThread = memory.read(BOOT_PARAMETERS_ADDRESS+8);
+        /*
+         * Find the TIB object
+         */
+        int bootThreadTib = memory.read(bootThread-12);
+        int bootThreadRVMClass = memory.read(bootThreadTib);
+        tibType = memory.read(bootThreadTib-12);
         StringBuffer sb = new StringBuffer("Boot Parameters: ");
         sb.append(Integer.toHexString(jtoc)).append(' ').append(Integer.toHexString(stack));
-        sb.append(' ').append(Integer.toHexString(stack));
+        sb.append(' ').append(Integer.toHexString(bootThread));
+        System.out.println(sb);
+        sb.setLength(0);
+        sb.append("boot thread tib ").append(Integer.toHexString(bootThreadTib));
+        sb.append("\n tib type").append(Integer.toHexString(tibType));
         System.out.println(sb);
     }
 
+    public boolean isTib(int address)
+    {
+        return address == tibType;
+    }
+    
     private static void usage()
     {
         System.out.println("usage: ObjectReader <memory dump file>");
