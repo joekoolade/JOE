41365c232d76414f51b039627ffae4e110327cc6
Joe Kulig <kulandthegang@gmail.com>
2018-08-13 06:49:37 -0400

    WIP

--- jikesrvm-hg/tools/objectReader/src/org/jam/tools/MemberReference.java
+++ jikesrvm-hg/tools/objectReader/src/org/jam/tools/MemberReference.java
@@ -3,6 +3,10 @@ package org.jam.tools;
 public class MemberReference extends JObject
 {
 
+    private final static int TYPE_REF_OFFSET = -4;
+    private final static int NAME_OFFSET = 0;
+    private final static int DESCRIPTOR_OFFSET = 4;
+    
     public MemberReference(MemoryReader memory, int address)
     {
         super(memory, address);
