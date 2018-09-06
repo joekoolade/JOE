f291e08a32bab4a8f8fd81855fd8b866015351e1
Joe Kulig <kulandthegang@gmail.com>
2018-09-06 06:54:22 -0400

    WIP

--- jikesrvm-hg/tools/objectReader/src/org/jam/tools/Lexer.java
+++ jikesrvm-hg/tools/objectReader/src/org/jam/tools/Lexer.java
@@ -1,6 +1,19 @@
 package org.jam.tools;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lexer
 {
-
+    public List<Token> lex(String input)
+    {
+        List<Token> result = new ArrayList<Token>();
+        String[] atoms = input.split(" \t");
+        foreach(String atom : atoms)
+        {
+            char aChar = input.charAt(index);
+            if(Character.isDigit(aChar))
+        }
+        return result;
+    }
 }
