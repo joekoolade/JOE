958a1f75788cd4b82f81d98b461a0f7e1f0d5d9a
Joseph Kulig <kulandthegang@gmail.com>
2022-05-02 07:08:24 -0400

    Turned off more debugging

--- rvm/src/org/jikesrvm/mm/mminterface/MemoryManager.java
+++ rvm/src/org/jikesrvm/mm/mminterface/MemoryManager.java
@@ -119,7 +119,7 @@ public final class MemoryManager {
    */
   @Interruptible
   public static void boot(BootRecord theBootRecord) {
-    Options.verbose.setValue(9);
+    Options.verbose.setValue(3);
     Extent pageSize = BootRecord.the_boot_record.bytesInPage;
     org.jikesrvm.runtime.Memory.setPageSize(pageSize);
     HeapLayout.mmapper.markAsMapped(BOOT_IMAGE_DATA_START, BOOT_IMAGE_DATA_SIZE);
